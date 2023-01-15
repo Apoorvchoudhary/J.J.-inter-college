fe32f8b30d3d713a9d2dda543da65f1a132826ff
Apoorvchoudhary <85571470+Apoorvchoudhary@users.noreply.github.com>
2023-01-15 18:00:46 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "admissionform";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+   // echo "connection ok";
+}
+else
+{
+    // echo "connection failed";
+}
+
+?>
+Footer
